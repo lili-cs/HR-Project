90979d89796d02a84b86cc77dd940e26360fb9f3
ShiyuLi1997 <40342681+ShiyuLi1997@users.noreply.github.com>
2022-09-11 20:50:55 -0400

    emailjs added to package json

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,20 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-// attach axios header if jwtToken exist in localStorage
-// supposed to be in frontend with axios.
-// (function() {
-//   const token = localStorage.getItem("jwtToken")
-//   console.log("jwtToken in immediate executed function: ", token)
-//   if (token) {
-//       axios.defaults.headers.common['Authorization'] = token;
-//   } else {
-//       axios.defaults.headers.common['Authorization'] = null;
-//       /*if setting null does not remove `Authorization` header then try     
-//         delete axios.defaults.headers.common['Authorization'];
-//       */
-//   }
-// })();
-
 module.exports = router;
